4f3f0c34724ed7c3902047d5bf105a6dfc36f3ec
unknown <asjoshi100@gmail.com>
2021-08-02 16:38:05 -0500

    Add existing file

---
+++ Transformation_matrix/transformation_vial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 15 18:50:45 2021
+
+@author: admin
+"""
+
+
+import numpy as np
+
+def transformation_vial(vial_num):
+    if vial_num == 1:
+        x = -22
+        y = -4.6
+        z = 19
+    if vial_num == 2:
+        x = -13.2
+        y = 4.5
+        z = 19
+    if vial_num == 3:
+        x = 5.5
+        y = 22.9
+        z = 19
+    if vial_num == 4:
+        x = 14.8
+        y = 32
+        z = 19
+        
+    return x, y, z
